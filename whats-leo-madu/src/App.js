0c3e77f44992ed0914a327b3c6549644f903607e
Leonardo Federmann <leofedermann@gmail.com>
2021-03-12 15:28:08 -0300

    Todas as funcionalidades obrigatórias estão ok

--- whats-leo-madu/src/App.js
+++ whats-leo-madu/src/App.js
@@ -1,56 +1,15 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
 import Form from './Components/Form'
-import styled from "styled-components";
+import styled from 'styled-components'
 
-const AppContainer = styled.div`
-  flex-direction: column;
-  max-width: 600px;
-  border: 1px solid black;
-  height: 100vh;
-  flex: 1;
-  display: flex;
-  
-`
 
-const MessagesContainer = styled.div`
-  flex: 1;
-  flex-direction: column;
-  display: flex;
-  justify-content: flex-end;
-  padding: 20px;
-`
-
-const BoldText = styled.span`
-  font-weight: bold;
-`
 
 class App extends React.Component {
 
-  addMessage = () => {
-
-    const newpost = {
-      nome: this.state.valorInputUser,
-      email: this.state.valorInputMessage,
-
-    };
-
-    const newPosts = [...this.state.inputs, newpost];
-    this.setState({ inputs: newPosts });
-  };
-
+  
   render() {
-    return (
-      <AppContainer>
-        <MessagesContainer>
-          {
-            this.state.inputs.map((message) =>
-              <p><BoldText>{message.inputUser} </BoldText>{': ' + message.inputMessage}</p>)
-          }
-        </MessagesContainer>
-        <Form addMessage={this.addMessage} />
-      </AppContainer>
-    );
+    return  <Form />
   }
 }
 
