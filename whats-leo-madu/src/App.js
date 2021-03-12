043e39aa27b71dbbf790c929931240d99b232b27
MaduBitencourt <bitencourtmadu@gmail.com>
2021-03-12 17:39:46 +0100

    add App.js changes

--- whats-leo-madu/src/App.js
+++ whats-leo-madu/src/App.js
@@ -1,25 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
+import Form from './Components/Form'
+import styled from "styled-components";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const AppContainer = styled.div`
+  flex-direction: column;
+  max-width: 600px;
+  border: 1px solid black;
+  height: 100vh;
+  flex: 1;
+  display: flex;
+  
+`
+
+const MessagesContainer = styled.div`
+  flex: 1;
+  flex-direction: column;
+  display: flex;
+  justify-content: flex-end;
+  padding: 20px;
+`
+
+const BoldText = styled.span`
+  font-weight: bold;
+`
+
+class App extends React.Component {
+
+  addMessage = () => {
+
+    const newpost = {
+      nome: this.state.valorInputUser,
+      email: this.state.valorInputMessage,
+
+    };
+
+    const newPosts = [...this.state.inputs, newpost];
+    this.setState({ inputs: newPosts });
+  };
+
+  render() {
+    return (
+      <AppContainer>
+        <MessagesContainer>
+          {
+            this.state.inputs.map((message) =>
+              <p><BoldText>{message.inputUser} </BoldText>{': ' + message.inputMessage}</p>)
+          }
+        </MessagesContainer>
+        <Form addMessage={this.addMessage} />
+      </AppContainer>
+    );
+  }
 }
 
 export default App;
